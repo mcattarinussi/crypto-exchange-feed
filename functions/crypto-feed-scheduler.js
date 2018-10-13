c0447141f50d8f339434cd492f3002278d2dd354
Mattia Cattarinussi <mcattarinussi@gmail.com>
2018-10-13 22:38:02 +0100

    Attach lambda to dynamodb stream

--- functions/crypto-feed-scheduler.js
+++ functions/crypto-feed-scheduler.js
@@ -1,5 +1,18 @@
 module.exports.handler = async (event, context) => {
-  return {
-    message: 'Hello! I am the cryptofeed scheduler'
+  console.log(`Received ${event.Records.length} records`);
+  for(record of event.Records) {
+    switch(record.eventName) {
+      case 'INSERT':
+        console.log(`Spawning new crypto-feed task for ${record.dynamodb.NewImage.Symbol.S}`);
+        break;
+      case 'REMOVE':
+        console.log(`Removing crypto-feed task for ${record.dynamodb.OldImage.Symbol.S}`);
+        break;
+      case 'MODIFY':
+        console.log(`MODIFY event, nothing to do for ${record.dynamodb.NewImage.Symbol.S}`);
+        break;
+      default:
+        console.log(`Unknown event type ${record.eventName}`);
+    }
   }
 }
