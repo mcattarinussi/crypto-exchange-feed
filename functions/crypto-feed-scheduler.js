1f91603d5d43e599ab33be32bbad982b1e9cbcca
Mattia Cattarinussi <mcattarinussi@gmail.com>
2018-10-14 20:00:16 +0100

    Implement stopTask function in lambda scheduler

--- functions/crypto-feed-scheduler.js
+++ functions/crypto-feed-scheduler.js
@@ -6,31 +6,47 @@ module.exports.handler = async (event, context) => {
   console.log(`Received ${event.Records.length} records`);
   for (record of event.Records) {
     switch (record.eventName) {
+
       case 'INSERT':
-        const cryptoCurrencySymbol = record.dynamodb.NewImage.Symbol.S;
-        console.log(`Spawning new crypto-feed task for ${cryptoCurrencySymbol}`);
+        const cryptoCurrencySymbolNew = record.dynamodb.NewImage.Symbol.S;
+        console.log(`Spawning new crypto-feed task for ${cryptoCurrencySymbolNew}`);
         try {
-          const data = await ECS.runTask(getRunTaskParams(cryptoCurrencySymbol)).promise();
-          console.log('runTask response', data);
-        } catch(err) {
+          const { tasks } = await ECS.runTask(buildRunTaskParams(cryptoCurrencySymbolNew)).promise();
+          console.log(`New task launched for ${cryptoCurrencySymbolNew}. TaskArn:`, tasks[0].taskArn);
+        } catch (err) {
           console.log(err);
         }
         break;
+
       case 'REMOVE':
-        console.log(`Removing crypto-feed task for ${record.dynamodb.OldImage.Symbol.S}`);
+        const cryptoCurrencySymbolOld = record.dynamodb.OldImage.Symbol.S;
+        console.log(`Removing crypto-feed task for ${cryptoCurrencySymbolOld}`);
+        try {
+          // TODO: handle null response
+          const { taskArn } = await getRunningTask(cryptoCurrencySymbolOld);
+          await ECS.stopTask({
+            cluster: process.env.CRYPTOFEEDS_ECS_CLUSTER,
+            task: taskArn,
+            reason: 'CRYPTOCURRENCY_REMOVED'
+          }).promise();
+          console.log(`Successfully removed task for ${cryptoCurrencySymbolOld}. Was taskArn ${taskArn}`);
+        } catch (err) {
+          console.log(err);
+        }
         break;
+
       case 'MODIFY':
         console.log(`MODIFY event, nothing to do for ${record.dynamodb.NewImage.Symbol.S}`);
         break;
+
       default:
         console.log(`Unknown event type ${record.eventName}`);
     }
   }
 }
 
-// The task will be named with the crypto currency symbol as suffix
-// and will receive the crypto currency symbol as an env variable
-const getRunTaskParams = cryptoCurrencySymbol => ({
+// The task will receive the crypto currency symbol as an env variable
+const buildRunTaskParams = cryptoCurrencySymbol => ({
   taskDefinition: process.env.CRYPTOFEED_ECS_TASK_DEFINITION,
   cluster: process.env.CRYPTOFEEDS_ECS_CLUSTER,
   count: 1,
@@ -58,3 +74,14 @@ const getRunTaskParams = cryptoCurrencySymbol => ({
     ]
   }
 });
+
+// Returns the running task object for the provided cryptoCurrencySymbol
+// or null if no matching task is found
+const getRunningTask = async cryptoCurrencySymbol => {
+  const { taskArns } = await ECS.listTasks({ cluster: process.env.CRYPTOFEEDS_ECS_CLUSTER }).promise();
+  const { tasks } = await ECS.describeTasks({
+    cluster: process.env.CRYPTOFEEDS_ECS_CLUSTER,
+    tasks: taskArns
+  }).promise();
+  return tasks.find(t => t.group === `crypto-feed-${cryptoCurrencySymbol}`) || null;
+};
