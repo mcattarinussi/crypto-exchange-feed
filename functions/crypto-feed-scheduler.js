17f506f79dc356ee45a1f31f8536234a0831c6ee
Mattia Cattarinussi <mcattarinussi@gmail.com>
2018-10-14 18:44:52 +0100

    Implement fargate task scheduler

--- functions/crypto-feed-scheduler.js
+++ functions/crypto-feed-scheduler.js
@@ -4,10 +4,17 @@ const ECS = new AWS.ECS();
 
 module.exports.handler = async (event, context) => {
   console.log(`Received ${event.Records.length} records`);
-  for(record of event.Records) {
-    switch(record.eventName) {
+  for (record of event.Records) {
+    switch (record.eventName) {
       case 'INSERT':
-        console.log(`Spawning new crypto-feed task for ${record.dynamodb.NewImage.Symbol.S}`);
+        const cryptoCurrencySymbol = record.dynamodb.NewImage.Symbol.S;
+        console.log(`Spawning new crypto-feed task for ${cryptoCurrencySymbol}`);
+        try {
+          const data = await ECS.runTask(getRunTaskParams(cryptoCurrencySymbol)).promise();
+          console.log('runTask response', data);
+        } catch(err) {
+          console.log(err);
+        }
         break;
       case 'REMOVE':
         console.log(`Removing crypto-feed task for ${record.dynamodb.OldImage.Symbol.S}`);
@@ -19,6 +26,33 @@ module.exports.handler = async (event, context) => {
         console.log(`Unknown event type ${record.eventName}`);
     }
   }
-  const { taskArns } = await ECS.listTasks({ cluster: process.env.CRYPTOFEEDS_ECS_CLUSTER }).promise();
-  console.log('Active ecs tasks: ', taskArns);
 }
+
+// The task will be named with the crypto currency symbol as suffix
+// and will receive the crypto currency symbol as an env variable
+const getRunTaskParams = cryptoCurrencySymbol => ({
+  taskDefinition: process.env.CRYPTOFEED_ECS_TASK_DEFINITION,
+  cluster: process.env.CRYPTOFEEDS_ECS_CLUSTER,
+  count: 1,
+  launchType: 'FARGATE',
+  networkConfiguration: {
+    awsvpcConfiguration: {
+      subnets: process.env.CRYPTOFEED_TASK_SUBNETS.split(','),
+      assignPublicIp: 'DISABLED',
+      securityGroups: process.env.CRYPTOFEED_TASK_SEC_GROUPS.split(',')
+    },
+  },
+  overrides: {
+    containerOverrides: [
+      {
+        name: process.env.CRYPTOFEED_CONTAINER_NAME,
+        environment: [
+          {
+            name: 'CRYPTO_CURRENCY_SYMBOL',
+            value: cryptoCurrencySymbol
+          }
+        ]
+      }
+    ]
+  }
+});
